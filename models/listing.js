bc63b341b67a84abd13d5ae727ad6f38328c48d0
shanumi007 <138568654+shanumi007@users.noreply.github.com>
2024-02-16 20:26:22 +0530

    Create listing.js

---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
